442c1a5cf16adead1fc530d213aed0ab7a5938bc
vagarioustoast <vagarioustoast@gmail.com>
2019-04-15 12:38:18 -0700

    adjusts readability to js file

--- js/main.js
+++ js/main.js
@@ -4,20 +4,25 @@ let addButton = document.getElementById("increment");
 let subtractButton = document.getElementById("decrement");
 let count = 0;
 
+// Subtract Button Attribute
 addButton.addEventListener("keyup", e => {
   incrementValue.setAttribute("value", incrementValue.value);
 });
 
-subtractButton.addEventListener("keyup", e => {
-  incrementValue.setAttribute("value", incrementValue.value);
-});
-
+// Subtract Button Functionality
 addButton.addEventListener("click", e => {
   incrementValue.value === ""
     ? null
     : (count += parseInt(incrementValue.value, 10));
   counter.innerHTML = count;
 });
+
+// Subtract Button Attribute
+subtractButton.addEventListener("keyup", e => {
+  incrementValue.setAttribute("value", incrementValue.value);
+});
+
+// Subtract Button Functionality
 subtractButton.addEventListener("click", e => {
   incrementValue.value === ""
     ? null
